5fa0a211b080eb188c6117423a14cb2cf9fb5247
Stephany Ramirez <stephany.ramireze@gmail.com>
2022-10-26 23:36:53 -0700

    Change margin in the image and text a little in the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/about.css'
-import aboutPic from '../assets/about-pic.jpg'
+import aboutPic from '../assets/about-pic.png'
 
 function About() {
   return (
@@ -9,9 +9,12 @@ function About() {
         <img src={aboutPic} alt="" />
       </div>
       <div className='aboutText'>
-        <h1 className='aboutTitle'>Hello there!  <br /> My name is Stephany Ramirez and I'm a software developer. </h1>
-        <p className='aboutBody'>I am a mechatronics engineer. I have always love coding, the challenges it imposes and seeing the final product gives me a great feeling of accomplishment.
-  That's the reason why I decided to do Le Wagon's Web Development bootcamp, and since the beginning I felt that I got what I was missing, I love doing things you cannot belief possible.</p>
+        <h1 className='aboutTitle'> About Me </h1>
+        <p className='aboutBody'>I have always love coding, the challenges it imposes and seeing the final product gives me a great feeling of accomplishment.
+          After five years as Mechatronic Engineer I decided to refocus my skills and become a Web Developer through Le Wagon's Bootcamp in Barcelona.<br></br><br />
+          I enjoy the more interactive part of coding, and doing things you cannot believe possible. I am a knowledge seeker and good at problem solving.
+          I spend my free time reading, baking, and I love traveling and meeting people from differents cultures.
+          </p>
       </div>
     </div>
   )
